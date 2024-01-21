117b688c618678e829abb6d185bf46d31d22dbc2
PRASANTH REDDY <prasanthreddy@PRASANTHs-MacBook-Air.local>
2024-01-22 01:13:15 +0530

    day-3

--- src/feedback.js
+++ src/feedback.js
@@ -1,6 +1,133 @@
-const FeedBack=()=>{
-    return <div>
-        <h1>FeedBack</h1>
+const FeedBack = () => {
+  return (
+    <div className="feedback">
+      <div className="feed-flex">
+        <div className="feed-item">
+          <div className="feed-content">
+            <p>
+              Bursting with imagery, motion, interaction and distraction though
+              it is, today’s World Wide Web is still primarily a conduit for
+              textual information. In HTML5, the focus on writing and authorship
+              is more pronounced than ever. It’s evident in the very way that
+              new elements such as article and aside are named. HTML5 asks us to
+              treat the HTML document more as… well, a document.
+            </p>
+          </div>
+          <hr></hr>
+          <div className="feed-bio">
+            <img src="https://source.unsplash.com/1600x800/?girl face"></img>
+            <div>
+              <h3>prasanthreddy</h3>
+              <p><i class="bi bi-instagram"></i> prasanthreddy903</p>
+            </div>
+          </div>
+        </div>
+        <div className="feed-item">
+          <div className="feed-content">
+            <p>
+              Bursting with imagery, motion, interaction and distraction though
+              it is, today’s World Wide Web is still primarily a conduit for
+              textual information. In HTML5, the focus on writing and authorship
+              is more pronounced than ever. It’s evident in the very way that
+              new elements such as article and aside are named. HTML5 asks us to
+              treat the HTML document more as… well, a document.
+            </p>
+          </div>
+          <hr></hr>
+          <div className="feed-bio">
+            <img src="https://source.unsplash.com/1600x800/?girl face"></img>
+            <div>
+              <h3>prasanthreddy</h3>
+              <p><i class="bi bi-instagram"></i> prasanthreddy903</p>
+            </div>
+          </div>
+        </div>
+        <div className="feed-item">
+          <div className="feed-content">
+            <p>
+              Bursting with imagery, motion, interaction and distraction though
+              it is, today’s World Wide Web is still primarily a conduit for
+              textual information. In HTML5, the focus on writing and authorship
+              is more pronounced than ever. It’s evident in the very way that
+              new elements such as article and aside are named. HTML5 asks us to
+              treat the HTML document more as… well, a document.
+            </p>
+          </div>
+          <hr></hr>
+          <div className="feed-bio">
+            <img src="https://source.unsplash.com/1600x800/?girl face"></img>
+            <div>
+              <h3>prasanthreddy</h3>
+              <p><i class="bi bi-instagram"></i> prasanthreddy903</p>
+            </div>
+          </div>
+        </div>
+        <div className="feed-item">
+          <div className="feed-content">
+            <p>
+              Bursting with imagery, motion, interaction and distraction though
+              it is, today’s World Wide Web is still primarily a conduit for
+              textual information. In HTML5, the focus on writing and authorship
+              is more pronounced than ever. It’s evident in the very way that
+              new elements such as article and aside are named. HTML5 asks us to
+              treat the HTML document more as… well, a document.
+            </p>
+          </div>
+          <hr></hr>
+          <div className="feed-bio">
+            <img src="https://source.unsplash.com/1600x800/?girl face"></img>
+            <div>
+              <h3>prasanthreddy</h3>
+              <p><i class="bi bi-instagram"></i> prasanthreddy903</p>
+            </div>
+          </div>
+        </div>
+        <div className="feed-item">
+          <div className="feed-content">
+            <p>
+              Bursting with imagery, motion, interaction and distraction though
+              it is, today’s World Wide Web is still primarily a conduit for
+              textual information. In HTML5, the focus on writing and authorship
+              is more pronounced than ever. It’s evident in the very way that
+              new elements such as article and aside are named. HTML5 asks us to
+              treat the HTML document more as… well, a document.
+            </p>
+          </div>
+          <hr></hr>
+          <div className="feed-bio">
+            <img src="https://source.unsplash.com/1600x800/?girl face"></img>
+            <div>
+              <h3>prasanthreddy</h3>
+              <p><i class="bi bi-instagram"></i> prasanthreddy903</p>
+            </div>
+          </div>
+        </div>
+        <div className="feed-item">
+          <div className="feed-content">
+            <p>
+              Bursting with imagery, motion, interaction and distraction though
+              it is, today’s World Wide Web is still primarily a conduit for
+              textual information. In HTML5, the focus on writing and authorship
+              is more pronounced than ever. It’s evident in the very way that
+              new elements such as article and aside are named. HTML5 asks us to
+              treat the HTML document more as… well, a document.
+            </p>
+          </div>
+          <hr></hr>
+          <div className="feed-bio">
+            <img src="https://source.unsplash.com/1600x800/?girl face"></img>
+            <div>
+              <h3>prasanthreddy</h3>
+              <p><i class="bi bi-instagram"></i> prasanthreddy903</p>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+      <div className="add-feedback">
+        <i class="bi bi-plus-lg"></i>
+      </div>
     </div>
-}
+  );
+};
 export default FeedBack;
--- src/navbar.js
+++ src/navbar.js
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-// import MbNav from "./mbNav";
 const Navbar=()=>{
-    const [Class,setClass]=useState('mb-nav')
+    const [Class,setClass]=useState('mb-nav-none')
     const [into, setInto] = useState(<i className="bi bi-list"></i>);
-
 function handleCross() {
   setInto((prevIcon) =>
     prevIcon.props.className === "bi bi-list" ? (
@@ -13,25 +11,45 @@ function handleCross() {
       <i className="bi bi-list"></i>
     )
   );
+
+  setClass((prevClass) =>
+      prevClass === 'mb-nav' ? 'mb-nav-none' : 'mb-nav'
+  );
+}
+function DontShow(){
+    setClass((prevClass) =>
+    prevClass === 'mb-nav' ? 'mb-nav-none' : 'mb-nav'
+)
+
+setInto((prevIcon) =>
+prevIcon.props.className === "bi bi-list" ? (
+  <i className="bi bi-x-lg"></i>
+) : (
+  <i className="bi bi-list"></i>
+)
+)
+
 }
     return <><nav className="navbar">
         <h1 className="nav-name"><span className="s">S</span><span className="u">T</span><span className="u">N</span><span className="u">U</span><span className="u">P</span></h1>
         <ul className="nav-items">
-            <Link className="link-items" to='/'><li>Home</li></Link>
-            <Link className="link-items" to='/courses'><li>Courses</li></Link>
-            <Link className="link-items" to='/roadmap'><li>Road Map</li></Link>
-            <Link className="link-items" to='feedbacks'><li>Feedbacks</li></Link>
-            <Link className="link-items" to='/contact'><li>Contact</li></Link>
+            <Link  className="link-items" to='/'><li>Home</li></Link>
+            <Link  className="link-items" to='/courses'><li>Courses</li></Link>
+            <Link  className="link-items" to='/roadmap'><li>Road Map</li></Link>
+            <Link  className="link-items" to='feedbacks'><li>Feedbacks</li></Link>
+            <Link  className="link-items" to='/contact'><li>Contact</li></Link>
             <Link to='/signin'><button className="sign-in">Sign In</button></Link>
-            <button onClick={handleCross} className="hamburger">{into}</button>
+            <button  onClick={handleCross}className="hamburger">{into}</button>
         </ul>
     </nav>
+    <div className="mb-nav-parent">
     <div className={Class}>
-      <Link to="/" className="mb-top-items"><h4><i class="bi bi-house-fill"></i> Home</h4></Link>
-      <Link to="/courses" className="mb-top-items"><h4><i class="bi bi-person-plus-fill"></i> Courses</h4></Link>
-      <Link to="/roadmap" className="mb-top-items"><h4><i class="bi bi-person-walking"></i> RoadMap</h4></Link>
-      <Link to="/feedbacks" className="mb-top-items"><h4><i class="bi bi-plus-circle-fill"></i> Feedbacks</h4></Link>
-      <Link to="/contact" className="mb-top-items"><h4> <i class="bi bi-telephone-outbound-fill"></i> Contact</h4></Link>
+      <Link onClick={DontShow} to="/" className="mb-top-items"><h4><i class="bi bi-house-fill"></i> Home</h4></Link>
+      <Link onClick={DontShow} to="/courses" className="mb-top-items"><h4><i class="bi bi-person-plus-fill"></i> Courses</h4></Link>
+      <Link onClick={DontShow} to="/roadmap" className="mb-top-items"><h4><i class="bi bi-person-walking"></i> RoadMap</h4></Link>
+      <Link onClick={DontShow} to="/feedbacks" className="mb-top-items"><h4><i class="bi bi-plus-circle-fill"></i> Feedbacks</h4></Link>
+      <Link onClick={DontShow} to="/contact" className="mb-top-items"><h4> <i class="bi bi-telephone-outbound-fill"></i> Contact</h4></Link>
+    </div>
     </div>
     </>
 }
